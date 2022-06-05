6520025cca4a0446eba810cdbb5f7517dfd5ffc5
Jordan Chappell <jordan.chappell33@gmail.com>
2022-06-06 08:25:05 +1000

    fix(tests): update unit tests and snapshot

--- codebase/src/App.unit.test.js
+++ codebase/src/App.unit.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
 
 import App from './App';
 
@@ -8,9 +7,4 @@ test('Component matches snapshot', () => {
   const component = renderer.create(<App />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
-});
-
-test('Component renders as expected', () => {
-  const component = shallow(<App />);
-  expect(component.text()).toContain('React App Template');
 });
