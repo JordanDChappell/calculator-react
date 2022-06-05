482d4119ad146d25357f5a2496f0ac790544f017
Jordan Chappell <jordan.chappell33@gmail.com>
2022-06-06 09:01:54 +1000

    deploy(pages): basic gh-pages deployment

--- codebase/src/Utils/propTypes.js
+++
@@ -1,8 +0,0 @@
-import PropTypes from 'prop-types';
-
-const childrenPropType = PropTypes.oneOfType([
-  PropTypes.arrayOf(PropTypes.node),
-  PropTypes.node,
-]).isRequired;
-
-export default childrenPropType;
