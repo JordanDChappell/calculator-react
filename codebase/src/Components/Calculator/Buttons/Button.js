5d39ccc3e004b763e1dc52aeb613f7d3adb2cf1f
Jordan Chappell <jordan.chappell33@gmail.com>
2022-06-06 17:07:16 +1000

    feat(colours): different colours for operations based on brightness of primary colour

--- codebase/src/Components/Calculator/Buttons/Button.js
+++ codebase/src/Components/Calculator/Buttons/Button.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+
+/* Library */
 import childrenPropType from '../../../Utils/propTypes';
+import lightenDarkenColour from '../../../Utils/colourUtils';
 
 const StyledButton = styled.button`
   height: 4em;
@@ -11,12 +14,18 @@ const StyledButton = styled.button`
   border: 0px;
   border-radius: 2em;
   color: ${(props) => props.theme.primaryText};
-  background-color: ${(props) => props.theme.primary};
+  background-color: ${(props) =>
+    lightenDarkenColour(props.theme.primary, props.secondary ? -50 : 0)};
   grid-area: ${(props) => props.area};
 `;
 
-const Button = ({ area, shortcut, children, onClick }) => (
-  <StyledButton area={area} onClick={onClick} title={`Keyboard: ${shortcut}`}>
+const Button = ({ area, shortcut, secondary, children, onClick }) => (
+  <StyledButton
+    area={area}
+    secondary={secondary}
+    onClick={onClick}
+    title={`Keyboard: ${shortcut}`}
+  >
     {children}
   </StyledButton>
 );
@@ -24,6 +33,7 @@ const Button = ({ area, shortcut, children, onClick }) => (
 Button.propTypes = {
   area: PropTypes.string.isRequired,
   shortcut: PropTypes.string,
+  secondary: PropTypes.bool,
   children: childrenPropType,
   onClick: PropTypes.func,
 };
